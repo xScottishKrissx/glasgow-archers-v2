f50a3fa1b6df08064259ac60860abaec3d488bf5
xScottishKrissx <chrisdunne66@gmail.com>
2018-04-04 18:01:41 +0100

    Day 4 - Afternoon - Worked on Boxes, Add Content to Footer and Improved Login Area

--- assets/js.js
+++ assets/js.js
@@ -5,11 +5,15 @@ console.log("Jquery is working")
       var scroll = $(window).scrollTop();
       //console.log(scroll);
 
-      if (scroll >= 15) {
+      if (scroll >= 10) {
 
         $(".nav-wrapper").css("position","fixed");
+        $(".nav-wrapper").css("top","-16vh");
+        $("#myNav ul li").css("padding","1rem");
       }else{
         $(".nav-wrapper").css("position","relative");
+        $(".nav-wrapper").css("top","0px");
+        $("#myNav ul li").css("padding","2rem");
       }
   });
 
