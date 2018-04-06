ba09c80d79bae58097964ff6de9b4020ff7ca98e
xScottishKrissx <chrisdunne66@gmail.com>
2018-04-06 14:11:58 +0100

    Day 5 - Morning - Worked on Navbar and Boxes

--- assets/js.js
+++ assets/js.js
@@ -3,20 +3,70 @@ console.log("Jquery is working")
 
   $(window).on("scroll", function() {
       var scroll = $(window).scrollTop();
+      var windowWidth  = $(window).width();
       //console.log(scroll);
 
-      if (scroll >= 10) {
+      if (scroll >= 10 && windowWidth > 575.98) {
+        $(".nav-wrapper").css("top","-102px");
 
-        $(".nav-wrapper").css("position","fixed");
-        $(".nav-wrapper").css("top","-16vh");
-        $("#myNav ul li").css("padding","1rem");
-      }else{
-        $(".nav-wrapper").css("position","relative");
-        $(".nav-wrapper").css("top","0px");
-        $("#myNav ul li").css("padding","2rem");
-      }
+      }else if(scroll >= 10 && windowWidth < 575.98){
+       $(".nav-wrapper").css("top","-221px");
+
+     }else{
+       $(".nav-wrapper").css("top","-0px");
+     }
+
   });
 
 
+/*
+$(".boxes .container .row-1 >:nth-child(1)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
 
+$(".boxes .container .row-1 :nth-child(2)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+$(".boxes .container .row-2 :nth-child(1)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
+
+$(".boxes .container .row-2 :nth-child(2)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+$(".boxes .container .row-2 :nth-child(3)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
+
+$(".boxes .container .row-2 :nth-child(4)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+$(".boxes .container .row-2 :nth-child(5)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
+
+$(".boxes .container .row-2 :nth-child(6)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+$(".boxes .container .row-2 :nth-child(7)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
+
+$(".boxes .container .row-2 :nth-child(8)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+$(".boxes .container .row-2 :nth-child(9)").hover(function(){
+    $(".boxes").toggleClass("red");
+});
+
+$(".boxes .container .row-2 :nth-child(10)").hover(function(){
+    $(".boxes").toggleClass("blue");
+});
+
+*/
 });
